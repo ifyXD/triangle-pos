487c2cf319a8a09340efa45866eb489ca2292312
EJade Picazo <125955532+mpicazo88@users.noreply.github.com>
2024-01-31 14:53:26 +0800

    hait

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -26,54 +26,7 @@
         <div class="col-md-5"> 
             <div class="card p-4 border-0 shadow-sm">
                 <div class="card-body">
-                    <form id="login" method="post" action="{{ url('/login') }}">
-                        @csrf
-                        <h1>Login</h1>
-                        <p class="text-muted">Sign In to your account</p>
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                      <i class="bi bi-person"></i>
-                                    </span>
-                            </div>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                   name="email" value="super.admin@test.com"
-                                   placeholder="Email">
-                            @error('email')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="input-group mb-4">
-                            <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                      <i class="bi bi-lock"></i>
-                                    </span>
-                            </div>
-                            <input id="password" type="password"
-                                   class="form-control @error('password') is-invalid @enderror" value="12345678"
-                                   placeholder="Password" name="password">
-                            @error('password')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="row">
-                            <div class="col-4">
-                                <button id="submit" class="btn btn-primary px-4 d-flex align-items-center"
-                                        type="submit">
-                                    Login
-                                    <div id="spinner" class="spinner-border text-info" role="status"
-                                         style="height: 20px;width: 20px;margin-left: 5px;display: none;">
-                                        <span class="sr-only">Loading...</span>
-                                    </div>
-                                </button>
-                            </div>
-                            <div class="col-8 text-right">
-                                <a class="btn btn-link px-0" href="{{ route('password.request') }}">
-                                    Forgot password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                     <h1 class="text-center">This is fucking homepage well played bro</h1>
                 </div>
             </div>
 
