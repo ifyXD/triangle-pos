de8174d74f7410fc1c0f0eff22bccabd41771747
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2024-03-28 22:09:30 +0800

    sige, gubaonn nato

--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -7,12 +7,13 @@
 */
 
 use App\Http\Middleware\CheckPermission;
+use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'], function () {
     //Print Barcode
     Route::get('/products/print-barcode', 'BarcodeController@printBarcode')->name('barcode.print');
     Route::get('/printToExcel/{id}', 'ProductController@toExcel')->name('category.printToExcel.id');
-    //Product 
+    //Product
     // Route::middleware([CheckPermission::class . ':create_products'])->group(function () {
     //     Route::get('products/create', 'ProductController@create');
     // });
@@ -21,16 +22,15 @@ Route::group(['middleware' => 'auth'], function () {
     // });
     // Route::middleware([CheckPermission::class . ':show_products'])->group(function () {
     //     Route::get('products/{id}', 'ProductController@show');
-    // });  
+    // });
     // Route::middleware([CheckPermission::class . ':access_products'])->group(function () {
     //     Route::resource('products', 'ProductController')->only('store','index','create');
     // });
     Route::resource('products', 'ProductController');
-  
+
 
     Route::middleware([CheckPermission::class . ':access_product_categories'])->group(function () {
-        //Product Category 
+        //Product Category
         Route::resource('product-categories', 'CategoriesController')->except('create', 'show');
     });
 });
-    
