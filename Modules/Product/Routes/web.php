bc9243962423c11f41cfcc86405f92e45ff5bf10
ricogoron <121242796+ricogoron@users.noreply.github.com>
2024-03-30 06:58:02 +0800

    asd

--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -13,22 +13,23 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/products/print-barcode', 'BarcodeController@printBarcode')->name('barcode.print');
     Route::get('/printToExcel/{id}', 'ProductController@toExcel')->name('category.printToExcel.id');
     //Product 
-    Route::middleware([CheckPermission::class . ':create_products'])->group(function () {
-        Route::get('products/create', 'ProductController@create');
-    });
-    Route::middleware([CheckPermission::class . ':edit_products'])->group(function () {
-        Route::get('products/{id}/edit', 'ProductController@edit');
-    });
-    Route::middleware([CheckPermission::class . ':show_products'])->group(function () {
-        Route::get('products/{id}', 'ProductController@show');
-    });  
-    Route::middleware([CheckPermission::class . ':access_products'])->group(function () {
-        Route::resource('products', 'ProductController')->only('store','index','create');
-    });
+    // Route::middleware([CheckPermission::class . ':create_products'])->group(function () {
+    //     Route::get('products/create', 'ProductController@create');
+    // });
+    // Route::middleware([CheckPermission::class . ':edit_products'])->group(function () {
+    //     Route::get('products/{id}/edit', 'ProductController@edit');
+    // });
+    // Route::middleware([CheckPermission::class . ':show_products'])->group(function () {
+    //     Route::get('products/{id}', 'ProductController@show');
+    // });  
+    // Route::middleware([CheckPermission::class . ':access_products'])->group(function () {
+    //     Route::resource('products', 'ProductController')->only('store','index','create');
+    // });
+    Route::resource('products', 'ProductController');
   
 
-    Route::middleware([CheckPermission::class . ':access_product_categories'])->group(function () {
+    // Route::middleware([CheckPermission::class . ':access_product_categories'])->group(function () {
         //Product Category 
         Route::resource('product-categories', 'CategoriesController')->except('create', 'show');
-    });
+    // });
 });
