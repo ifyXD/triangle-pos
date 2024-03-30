d7f9add0e04c893008dce20826c5a75ba209bf07
ricogoron <121242796+ricogoron@users.noreply.github.com>
2024-03-30 18:01:45 +0800

    asdsdsd

--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -12,12 +12,12 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'auth'], function () {
     //Print Barcode
     Route::get('/products/print-barcode', 'BarcodeController@printBarcode')->name('barcode.print');
-    Route::get('/printToExcel/{id}', 'ProductController@toExcel')->name('category.printToExcel.id'); 
+    Route::get('/printToExcel/{id}', 'ProductController@toExcel')->name('category.printToExcel.id');
     Route::resource('products', 'ProductController');
 
 
-    Route::middleware([CheckPermission::class . ':access_product_categories'])->group(function () {
-        //Product Category
-        Route::resource('product-categories', 'CategoriesController')->except('create', 'show');
+    // Route::middleware([CheckPermission::class . ':access_product_categories'])->group(function () {
+    //Product Category
+    Route::resource('product-categories', 'CategoriesController')->except('create', 'show');
     // });
 });
--- Modules/User/Routes/web.php
+++ Modules/User/Routes/web.php
@@ -9,9 +9,9 @@
 Route::group(['middleware' => 'auth'], function () {
 
     //User Profile
-    Route::get('/user/profile', 'ProfileController@edit')->name('profile.edit');
-    Route::patch('/user/profile', 'ProfileController@update')->name('profile.update');
-    Route::patch('/user/password', 'ProfileController@updatePassword')->name('profile.update.password');
+    Route::get('/edit-user/profile', 'ProfileController@edit')->name('profile.edit');
+    Route::patch('/edit-user/profile', 'ProfileController@update')->name('profile.update');
+    Route::patch('/edit-user/password', 'ProfileController@updatePassword')->name('profile.update.password');
 
     //Users
     Route::resource('users', 'UsersController')->except('show');
