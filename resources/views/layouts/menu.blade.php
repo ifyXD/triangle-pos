907e76001cc6918314ae9a953bc64273d82814bf
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2024-04-09 19:24:37 +0800

    waa uy

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-{{--<span class="kami-kami-span">Main</span>--}}
+<span class="kami-kami-span">General</span>
 <li class="c-sidebar-nav-item {{ request()->routeIs('home') ? 'c-active' : '' }}">
     <a class="c-sidebar-nav-link" href="{{ route('home') }}">
         <i class="c-sidebar-nav-icon bi bi-house" style="line-height: 1;"></i> Home
@@ -6,7 +6,8 @@
 </li>
 
 @if (auth()->user()->hasAccessToPermission('access_products'))
-    <span class="kami-kami-span">Inventory</span>
+{{--    <span class="kami-kami-span">Inventory</span>--}}
+
     <li
         class="c-sidebar-nav-item c-sidebar-nav-dropdown {{ request()->routeIs('products.*') || request()->routeIs('product-categories.*') ? 'c-show' : '' }}">
         <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
@@ -16,7 +17,7 @@
             @if (auth()->user()->hasAccessToPermission('access_product_categories'))
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('product-categories.*') ? 'c-active' : '' }}"
-                        href="{{ route('product-categories.index') }}">
+                       href="{{ route('product-categories.index') }}">
                         <i class="c-sidebar-nav-icon bi bi-collection" style="line-height: 1;"></i> Categories
                     </a>
                 </li>
@@ -24,7 +25,7 @@
             @if (auth()->user()->hasAccessToPermission('create_products'))
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('products.create') ? 'c-active' : '' }}"
-                        href="{{ route('products.create') }}">
+                       href="{{ route('products.create') }}">
                         <i class="c-sidebar-nav-icon bi bi-journal-plus" style="line-height: 1;"></i> Create Product
                     </a>
                 </li>
@@ -32,14 +33,14 @@
             @if (auth()->user()->hasAccessToPermission('access_products'))
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('products.index') ? 'c-active' : '' }}"
-                        href="{{ route('products.index') }}">
+                       href="{{ route('products.index') }}">
                         <i class="c-sidebar-nav-icon bi bi-journals" style="line-height: 1;"></i> All Products
                     </a>
                 </li>
             @endif
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('units.index') ? 'c-active' : '' }}"
-                    href="{{ route('units.index') }}">
+                   href="{{ route('units.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-calculator" style="line-height: 1;"></i> Units
                 </a>
             </li>
@@ -64,14 +65,14 @@
             @if (auth()->user()->hasAccessToPermission('create_adjustments'))
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('adjustments.create') ? 'c-active' : '' }}"
-                        href="{{ route('adjustments.create') }}">
+                       href="{{ route('adjustments.create') }}">
                         <i class="c-sidebar-nav-icon bi bi-journal-plus" style="line-height: 1;"></i> Create Adjustment
                     </a>
                 </li>
             @endif
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('adjustments.index') ? 'c-active' : '' }}"
-                    href="{{ route('adjustments.index') }}">
+                   href="{{ route('adjustments.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-journals" style="line-height: 1;"></i> All Adjustments
                 </a>
             </li>
@@ -155,7 +156,7 @@
 {{--@endif--}}
 
 @if (auth()->user()->hasAccessToPermission('access_sales'))
-<span class="kami-kami-span">Sales</span>
+{{--    <span class="kami-kami-span">Sales</span>--}}
     <li class="c-sidebar-nav-item c-sidebar-nav-dropdown {{ request()->routeIs('sales.*') || request()->routeIs('sale-payments*') ? 'c-show' : '' }}">
         <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
             <i class="c-sidebar-nav-icon bi bi-receipt" style="line-height: 1;"></i> Sales
@@ -164,7 +165,7 @@
             <ul class="c-sidebar-nav-dropdown-items">
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('sales.create') ? 'c-active' : '' }}"
-                        href="{{ route('sales.create') }}">
+                       href="{{ route('sales.create') }}">
                         <i class="c-sidebar-nav-icon bi bi-journal-plus" style="line-height: 1;"></i> Create Sale
                     </a>
                 </li>
@@ -173,7 +174,7 @@
         <ul class="c-sidebar-nav-dropdown-items">
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('sales.index') ? 'c-active' : '' }}"
-                    href="{{ route('sales.index') }}">
+                   href="{{ route('sales.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-journals" style="line-height: 1;"></i> All Sales
                 </a>
             </li>
@@ -190,7 +191,7 @@
             <ul class="c-sidebar-nav-dropdown-items">
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('sale-returns.create') ? 'c-active' : '' }}"
-                        href="{{ route('sale-returns.create') }}">
+                       href="{{ route('sale-returns.create') }}">
                         <i class="c-sidebar-nav-icon bi bi-journal-plus" style="line-height: 1;"></i> Create Sale
                         Return
                     </a>
@@ -200,7 +201,7 @@
         <ul class="c-sidebar-nav-dropdown-items">
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('sale-returns.index') ? 'c-active' : '' }}"
-                    href="{{ route('sale-returns.index') }}">
+                   href="{{ route('sale-returns.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-journals" style="line-height: 1;"></i> All Sale Returns
                 </a>
             </li>
@@ -208,7 +209,7 @@
     </li>
 @endif
 @if (auth()->user()->hasAccessToPermission('access_expenses'))
-    <span class="kami-kami-span">Expense</span>
+{{--    <span class="kami-kami-span">Expense</span>--}}
     <li
         class="c-sidebar-nav-item c-sidebar-nav-dropdown {{ request()->routeIs('expenses.*') || request()->routeIs('expense-categories.*') ? 'c-show' : '' }}">
         <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
@@ -218,7 +219,7 @@
             @if (auth()->user()->hasAccessToPermission('access_expense_categories'))
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('expense-categories.*') ? 'c-active' : '' }}"
-                        href="{{ route('expense-categories.index') }}">
+                       href="{{ route('expense-categories.index') }}">
                         <i class="c-sidebar-nav-icon bi bi-collection" style="line-height: 1;"></i> Categories
                     </a>
                 </li>
@@ -226,14 +227,14 @@
             @if (auth()->user()->hasAccessToPermission('create_expenses'))
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('expenses.create') ? 'c-active' : '' }}"
-                        href="{{ route('expenses.create') }}">
+                       href="{{ route('expenses.create') }}">
                         <i class="c-sidebar-nav-icon bi bi-journal-plus" style="line-height: 1;"></i> Create Expense
                     </a>
                 </li>
             @endif
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('expenses.index') ? 'c-active' : '' }}"
-                    href="{{ route('expenses.index') }}">
+                   href="{{ route('expenses.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-journals" style="line-height: 1;"></i> All Expenses
                 </a>
             </li>
@@ -253,7 +254,7 @@
             @if (auth()->user()->hasAccessToPermission('access_customers'))
                 <li class="c-sidebar-nav-item">
                     <a class="c-sidebar-nav-link {{ request()->routeIs('customers.*') ? 'c-active' : '' }}"
-                        href="{{ route('customers.index') }}">
+                       href="{{ route('customers.index') }}">
                         <i class="c-sidebar-nav-icon bi bi-people-fill" style="line-height: 1;"></i> Customers
                     </a>
                 </li>
@@ -278,7 +279,7 @@
         <ul class="c-sidebar-nav-dropdown-items">
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('profit-loss-report.index') ? 'c-active' : '' }}"
-                    href="{{ route('profit-loss-report.index') }}">
+                   href="{{ route('profit-loss-report.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-clipboard-data" style="line-height: 1;"></i> Profit / Loss
                     Report
                 </a>
@@ -291,13 +292,13 @@
             </li> --}}
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('sales-report.index') ? 'c-active' : '' }}"
-                    href="{{ route('sales-report.index') }}">
+                   href="{{ route('sales-report.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-clipboard-data" style="line-height: 1;"></i> Sales Report
                 </a>
             </li>
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('purchases-report.index') ? 'c-active' : '' }}"
-                    href="{{ route('purchases-report.index') }}">
+                   href="{{ route('purchases-report.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-clipboard-data" style="line-height: 1;"></i> Purchases Report
                 </a>
             </li>
@@ -326,19 +327,19 @@
         <ul class="c-sidebar-nav-dropdown-items">
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('users.create') ? 'c-active' : '' }}"
-                    href="{{ route('users.create') }}">
+                   href="{{ route('users.create') }}">
                     <i class="c-sidebar-nav-icon bi bi-person-plus" style="line-height: 1;"></i> Create User
                 </a>
             </li>
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('users*') ? 'c-active' : '' }}"
-                    href="{{ route('users.index') }}">
+                   href="{{ route('users.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-person-lines-fill" style="line-height: 1;"></i> All Users
                 </a>
             </li>
             <li class="c-sidebar-nav-item">
                 <a class="c-sidebar-nav-link {{ request()->routeIs('roles*') ? 'c-active' : '' }}"
-                    href="{{ route('roles.index') }}">
+                   href="{{ route('roles.index') }}">
                     <i class="c-sidebar-nav-icon bi bi-key" style="line-height: 1;"></i> Roles & Permissions
                 </a>
             </li>
@@ -346,39 +347,39 @@
     </li>
 @endcan
 {{--<span class="kami-kami-span">Settings</span>--}}
-    <li  class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-        <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-            <i class="c-sidebar-nav-icon bi bi-gear" style="line-height: 1;"></i> Settings
-        </a>
-        @if (auth()->user()->hasRole('Super Admin'))
-            <ul class="c-sidebar-nav-dropdown-items">
-                <li class="c-sidebar-nav-item">
-                    <a class="c-sidebar-nav-link {{ request()->routeIs('currencies*') ? 'c-active' : '' }}"
-                        href="{{ route('currencies.index') }}">
-                        <i class="c-sidebar-nav-icon bi bi-cash-stack" style="line-height: 1;"></i> Currencies
-                    </a>
-                </li>
-            </ul>
-        @endif
-        {{-- @can('access_settings')
-            <ul class="c-sidebar-nav-dropdown-items">
-                <li class="c-sidebar-nav-item">
-                    <a class="c-sidebar-nav-link {{ request()->routeIs('settings*') ? 'c-active' : '' }}"
-                        href="{{ route('settings.index') }}">
-                        <i class="c-sidebar-nav-icon bi bi-sliders" style="line-height: 1;"></i> System Settings
-                    </a>
-                </li>
-            </ul>
-        @endcan --}}
-        {{-- user settings --}}
-        @if (!auth()->user()->hasRole('Super Admin'))
-            <ul class="c-sidebar-nav-dropdown-items">
-                <li class="c-sidebar-nav-item">
-                    <a class="c-sidebar-nav-link {{ request()->routeIs('system-settings*') ? 'c-active' : '' }}"
-                        href="{{ route('system-settings.index') }}">
-                        <i class="c-sidebar-nav-icon bi bi-cash-stack" style="line-height: 1;"></i>System Settings
-                    </a>
-                </li>
-            </ul>
-        @endif
-    </li>
+<li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
+    <a class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
+        <i class="c-sidebar-nav-icon bi bi-gear" style="line-height: 1;"></i> Settings
+    </a>
+    @if (auth()->user()->hasRole('Super Admin'))
+        <ul class="c-sidebar-nav-dropdown-items">
+            <li class="c-sidebar-nav-item">
+                <a class="c-sidebar-nav-link {{ request()->routeIs('currencies*') ? 'c-active' : '' }}"
+                   href="{{ route('currencies.index') }}">
+                    <i class="c-sidebar-nav-icon bi bi-cash-stack" style="line-height: 1;"></i> Currencies
+                </a>
+            </li>
+        </ul>
+    @endif
+    {{-- @can('access_settings')
+        <ul class="c-sidebar-nav-dropdown-items">
+            <li class="c-sidebar-nav-item">
+                <a class="c-sidebar-nav-link {{ request()->routeIs('settings*') ? 'c-active' : '' }}"
+                    href="{{ route('settings.index') }}">
+                    <i class="c-sidebar-nav-icon bi bi-sliders" style="line-height: 1;"></i> System Settings
+                </a>
+            </li>
+        </ul>
+    @endcan --}}
+    {{-- user settings --}}
+    @if (!auth()->user()->hasRole('Super Admin'))
+        <ul class="c-sidebar-nav-dropdown-items">
+            <li class="c-sidebar-nav-item">
+                <a class="c-sidebar-nav-link {{ request()->routeIs('system-settings*') ? 'c-active' : '' }}"
+                   href="{{ route('system-settings.index') }}">
+                    <i class="c-sidebar-nav-icon bi bi-cash-stack" style="line-height: 1;"></i>System Settings
+                </a>
+            </li>
+        </ul>
+    @endif
+</li>
