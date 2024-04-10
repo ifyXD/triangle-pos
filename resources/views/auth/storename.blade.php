fd48807fd23155373c20a9fa31ac35acab2720e4
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2024-04-10 19:11:05 +0800

    ok na settings?

--- resources/views/auth/storename.blade.php
+++ resources/views/auth/storename.blade.php
@@ -12,37 +12,40 @@
     {{--            </h1>--}}
     {{--        </form>--}}
     {{--    </div>--}}
-
+    <style>
+        input[type=text]:focus, textarea:focus {
+            width: 660px;
+            border: none;
+            border-bottom: 2px solid rgba(81, 203, 238, 1);
+        }
+    </style>
     <div class="store">
         <div class="container-fluid">
-                <form class="store-container" action="{{route('updaterequirements')}}" method="post" style="transform: translateY(clamp(10px, 20vh, 30vh));">
-                    @csrf
-                    <h1 class="headline">Let's give your store a <span>name</span> </h1>
-                    <div class="input-container">
-                        <label for="storename" class="visually-hidden">Store Name:</label>
-                        <input class="storename" type="text" id="storename" name="storename" required>
-                        <input type="hidden" name="requestdata" value="storename">
-                        <button class="button-next" type="submit">NEXT</button>
-                    </div>
+            <form class="store-container" action="{{route('updaterequirements')}}" method="post"
+                  style="transform: translateY(clamp(10px, 20vh, 30vh));">
+                @csrf
+                <h1 class="headline">Let's give your store a <span>name</span></h1>
+                <div class="input-container">
+                    <label for="storename" class="visually-hidden">Store Name:</label>
+                    <input class="storename" type="text" id="storename" name="storename" required>
+                    <input type="hidden" name="requestdata" value="storename">
+                    <button class="button-next" type="submit">NEXT</button>
+                </div>
 
 
-
-
-                    {{--<a class="button-back" href="{{ url("/") }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <mask id="mask0_262_436" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
-                                  height="24">
-                                <rect width="24" height="24" fill="#D9D9D9"/>
-                            </mask>
-                            <g mask="url(#mask0_262_436)">
-                                <path d="M10 22L0 12L10 2L11.775 3.775L3.55 12L11.775 20.225L10 22Z" fill="#1C1B1F"/>
-                            </g>
-                        </svg>
-                        <span>BACK</span>
-                    </a>--}}
-                </form>
-
-
+                {{--<a class="button-back" href="{{ url("/") }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <mask id="mask0_262_436" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24"
+                              height="24">
+                            <rect width="24" height="24" fill="#D9D9D9"/>
+                        </mask>
+                        <g mask="url(#mask0_262_436)">
+                            <path d="M10 22L0 12L10 2L11.775 3.775L3.55 12L11.775 20.225L10 22Z" fill="#1C1B1F"/>
+                        </g>
+                    </svg>
+                    <span>BACK</span>
+                </a>--}}
+            </form>
 
 
         </div>
