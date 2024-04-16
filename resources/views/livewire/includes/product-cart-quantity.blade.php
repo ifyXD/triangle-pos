86c467eb54fabb8fe8989e3370968d42b66c0d9e
ricogoron <121242796+ricogoron@users.noreply.github.com>
2024-04-16 13:01:49 +0800

    sale

--- resources/views/livewire/includes/product-cart-quantity.blade.php
+++ resources/views/livewire/includes/product-cart-quantity.blade.php
@@ -1,8 +1,3 @@
 <div class="input-group d-flex justify-content-center">
     <input  wire:model="quantity.{{ $cart_item->id }}" style="min-width: 40px;max-width: 90px;" type="number" class="form-control" value="{{ $cart_item->qty }}" min="1" max="{{$cart_item->options['stock']}}">
-    <div class="input-group-append">
-        <button type="button" wire:click="updateQuantity('{{ $cart_item->rowId }}', {{ $cart_item->id }})" class="btn btn-info">
-            <i class="bi bi-check"></i>
-        </button>
-    </div>
 </div>
--- resources/views/livewire/product-cart.blade.php
+++ resources/views/livewire/product-cart.blade.php
@@ -11,27 +11,28 @@
             </div>
         @endif
         <div class="table-responsive position-relative">
-            <div wire:loading.flex class="col-12 position-absolute justify-content-center align-items-center" style="top:0;right:0;left:0;bottom:0;background-color: rgba(255,255,255,0.5);z-index: 99;">
+            <div wire:loading.flex class="col-12 position-absolute justify-content-center align-items-center"
+                style="top:0;right:0;left:0;bottom:0;background-color: rgba(255,255,255,0.5);z-index: 99;">
                 <div class="spinner-border text-primary" role="status">
                     <span class="sr-only">Loading...</span>
                 </div>
             </div>
-            <table class="table table-bordered">
+            <table class="table table-bordered" id="cart_product">
                 <thead class="thead-dark">
-                <tr>
-                    <th class="align-middle">Product</th>
-                    <th class="align-middle text-center">Net Unit Price</th>
-                    <th class="align-middle text-center">Stock</th>
-                    <th class="align-middle text-center">Quantity</th>
-                    <th class="align-middle text-center">Discount</th>
-                    <th class="align-middle text-center">Tax</th>
-                    <th class="align-middle text-center">Sub Total</th>
-                    <th class="align-middle text-center">Action</th>
-                </tr>
+                    <tr>
+                        <th class="align-middle">Product</th>
+                        <th class="align-middle text-center">Price / Unit</th>
+                        <th class="align-middle text-center">Stock</th>
+                        <th class="align-middle text-center">Quantity</th>
+                        {{-- <th class="align-middle text-center">Discount</th>
+                    <th class="align-middle text-center">Tax</th> --}}
+                        <th class="align-middle text-center">Sub Total</th>
+                        <th class="align-middle text-center">Action</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    @if($cart_items->isNotEmpty())
-                        @foreach($cart_items as $cart_item)
+                    @if ($cart_items->isNotEmpty())
+                        @foreach ($cart_items as $cart_item)
                             <tr>
                                 <td class="align-middle">
                                     {{ $cart_item->name }} <br>
@@ -41,31 +42,39 @@
                                     @include('livewire.includes.product-cart-modal') --}}
                                 </td>
 
-                                <td x-data="{ open{{ $cart_item->id }}: false }" class="align-middle text-center">
-                                    <span x-show="!open{{ $cart_item->id }}" @click="open{{ $cart_item->id }} = !open{{ $cart_item->id }}">{{ format_currency($cart_item->price) }}</span>
-
-                                    <div x-show="open{{ $cart_item->id }}">
-                                        @include('livewire.includes.product-cart-price')
-                                    </div>
+                                <td class="align-middle text-center">
+                                    <input style="min-width: 40px;max-width: 100%;" type="text" readonly
+                                        id="priceValue_{{ $cart_item->id }}" class="form-control" min="0"
+                                        value="0">
+                                    <select name="unit_select_{{ $cart_item->id }}"
+                                        onchange="selectedUnit({{ $cart_item->id }}, $(this).val());"
+                                        id="unit_select_{{ $cart_item->id }}" style="min-width: 40px;max-width: 100%;"
+                                        class="form-control">
+                                        <option value="" disabled selected>Select Unit</option>
+                                        @foreach ($cart_item->options['prices'] as $priceOption)
+                                            <option value="{{ $priceOption['price'] }}">
+                                                {{ $priceOption['product_unit'] }}</option>
+                                        @endforeach
+                                    </select>
                                 </td>
 
                                 <td class="align-middle text-center text-center">
-                                    <span class="badge badge-info">{{ $cart_item->options->stock}}</span>
+                                    <span class="badge badge-info">{{ $cart_item->options->stock }}</span>
                                 </td>
 
                                 <td class="align-middle text-center">
-                                    @include('livewire.includes.product-cart-quantity')
+                                    {{-- @include('livewire.includes.product-cart-quantity') --}}
+                                    <div class="input-group d-flex justify-content-center">
+                                        <input id="qtyval_{{ $cart_item->id }}"
+                                            onchange="quantity({{ $cart_item->id }}, $(this).val());"
+                                            style="min-width: 40px;max-width: 90px;" type="number" class="form-control"
+                                            value="{{ $cart_item->qty }}" min="1"
+                                            max="{{ $cart_item->options['stock'] }}">
+                                    </div>
+
                                 </td>
 
-                                <td class="align-middle text-center">
-                                    {{ format_currency($cart_item->options->product_discount) }}
-                                </td>
-
-                                <td class="align-middle text-center">
-                                    {{ format_currency($cart_item->options->product_tax) }}
-                                </td>
-
-                                <td class="align-middle text-center">
+                                <td class="align-middle text-center tdClass" id="td_{{ $cart_item->id }}">
                                     {{ format_currency($cart_item->options->sub_total) }}
                                 </td>
 
@@ -79,9 +88,9 @@
                     @else
                         <tr>
                             <td colspan="8" class="text-center">
-                        <span class="text-danger">
-                            Please search & select products!
-                        </span>
+                                <span class="text-danger">
+                                    Please search & select products!
+                                </span>
                             </td>
                         </tr>
                     @endif
@@ -94,26 +103,11 @@
         <div class="col-md-4">
             <div class="table-responsive">
                 <table class="table table-striped">
-                    {{-- <tr>
-                        <th>Tax ({{ $global_tax }}%)</th>
-                        <td>(+) {{ format_currency(Cart::instance($cart_instance)->tax()) }}</td>
-                    </tr>
-                    <tr>
-                        <th>Discount ({{ $global_discount }}%)</th>
-                        <td>(-) {{ format_currency(Cart::instance($cart_instance)->discount()) }}</td>
-                    </tr>
-                    <tr>
-                        <th>Shipping</th>
-                        <input type="hidden" value="{{ $shipping }}" name="shipping_amount">
-                        <td>(+) {{ format_currency($shipping) }}</td>
-                    </tr> --}}
                     <tr>
                         <th>Grand Total</th>
-                        @php
-                            $total_with_shipping = Cart::instance($cart_instance)->total() + (float) $shipping
-                        @endphp
-                        <th>
-                            (=) {{ format_currency($total_with_shipping) }}
+                        
+                        <th id="grand_total">
+                            (=) 
                         </th>
                     </tr>
                 </table>
@@ -121,26 +115,51 @@
         </div>
     </div>
 
-    <input type="hidden" name="total_amount" value="{{ $total_with_shipping }}">
+    <input type="hidden" name="total_amount">
 
-    {{-- <div class="form-row">
-        <div class="col-lg-4">
-            <div class="form-group">
-                <label for="tax_percentage">Tax (%)</label>
-                <input wire:model.blur="global_tax" type="number" class="form-control" name="tax_percentage" min="0" max="100" value="{{ $global_tax }}" required>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="form-group">
-                <label for="discount_percentage">Discount (%)</label>
-                <input wire:model.blur="global_discount" type="number" class="form-control" name="discount_percentage" min="0" max="100" value="{{ $global_discount }}" required>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="form-group">
-                <label for="shipping_amount">Shipping</label>
-                <input wire:model.blur="shipping" type="number" class="form-control" name="shipping_amount" min="0" value="0" required step="0.01">
-            </div>
-        </div>
-    </div> --}}
 </div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script>
+    function grandTotal() {
+        var grandTotal = 0;
+
+        // Iterate through each <td> element in the table body
+        $(`#cart_product tbody .tdClass`).each(function() {
+            // Get the text content of the current <td>
+            var text = $(this).text();
+
+            // Remove the currency sign (₱) and any leading/trailing whitespace
+            var value = parseFloat(text.replace('₱', '').trim());
+
+            // Add the value to the grand total
+            if (!isNaN(value)) {
+                grandTotal += value;
+            }
+        });
+
+        // Set the grand total in your desired element
+        $('#grand_total').text('₱' + grandTotal.toFixed(2));
+        
+    }
+
+    function selectedUnit(id, price) {
+        let qty = $(`#qtyval_${id}`).val();
+        let sub_total = qty * price;
+        $(`#priceValue_${id}`).val(price);
+        $(`#td_${id}`).text('₱' + sub_total + '.00');
+        grandTotal();
+    }
+
+    function quantity(id, value) {
+        let price = $(`#priceValue_${id}`).val();
+        let sub_total = value * price;
+        $(`#td_${id}`).text('₱' + sub_total + '.00');
+
+        grandTotal();
+
+    }
+    // $(document).ready(function() {
+
+    // });
+</script>
