830639f6032eba735e428fa9891efedcdda73d67
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2024-04-20 03:34:26 +0800

    ok

--- Modules/Product/Resources/views/products/create.blade.php
+++ Modules/Product/Resources/views/products/create.blade.php
@@ -1,3 +1,4 @@
+@php use Modules\Setting\Entities\Unit; @endphp
 @extends('layouts.app')
 
 @section('title', 'Create Product')
@@ -27,9 +28,10 @@
                             <div class="form-row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_name">Product Name <span class="text-danger">*</span></label>
+                                        <label for="product_name">Product Name <span
+                                                class="text-danger">*</span></label>
                                         <input type="text" class="form-control" name="product_name" required
-                                            value="{{ old('product_name') }}">
+                                               value="{{ old('product_name') }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
@@ -43,7 +45,8 @@
                                                     </option>
                                                 @else
                                                     @if ($category->user_id == auth()->user()->id || $category->user_id == 1)
-                                                        <option value="{{ $category->id }}">{{ $category->category_name }}
+                                                        <option
+                                                            value="{{ $category->id }}">{{ $category->category_name }}
                                                         </option>
                                                     @endif
                                                 @endif
@@ -52,24 +55,25 @@
                                         </select>
                                         <div class="input-group-append d-flex">
                                             <button data-toggle="modal" data-target="#categoryCreateModal"
-                                                class="btn btn-outline-primary" type="button">
+                                                    class="btn btn-outline-primary" type="button">
                                                 Add
                                             </button>
                                         </div>
                                     </div>
-                                </div> 
+                                </div>
                             </div>
 
-                            <div class="form-row"> 
+                            <div class="form-row">
                             </div>
- 
+
 
                             <div class="form-row">
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <label for="product_quantity">Quantity <span class="text-danger">*</span></label>
+                                        <label for="product_quantity">Quantity <span
+                                                class="text-danger">*</span></label>
                                         <input type="number" class="form-control" name="product_quantity" required
-                                            value="{{ old('product_quantity') }}" min="1">
+                                               value="{{ old('product_quantity') }}" min="1">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
@@ -77,62 +81,59 @@
                                         <label for="product_stock_alert">Alert Quantity <span
                                                 class="text-danger">*</span></label>
                                         <input type="number" class="form-control" name="product_stock_alert" required
-                                            value="{{ old('product_stock_alert', 0) }}" min="0" max="100">
+                                               value="{{ old('product_stock_alert', 0) }}" min="0" max="100">
                                     </div>
                                 </div>
                             </div>
 
                             <div class="form-row">
-                                {{-- <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="product_order_tax">Tax (%)</label>
-                                        <input type="number" class="form-control" name="product_order_tax"
-                                            value="{{ old('product_order_tax') }}" min="1">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="product_tax_type">Tax type</label>
-                                        <select class="form-control" name="product_tax_type" id="product_tax_type">
-                                            <option value="" selected>Select Tax Type</option>
-                                            <option value="1">Exclusive</option>
-                                            <option value="2">Inclusive</option>
-                                        </select>
-                                    </div>
-                                </div> --}}
                                 <div class="col-md-6">
-                                    <label for="product_unit">Unit <i class="bi bi-question-circle-fill text-info"
-                                            data-toggle="tooltip" data-placement="top"
-                                            title="This short text will be placed after Product Quantity."></i> <span
-                                            class="text-danger">*</span></label>
+                                    <label for="product_unit">Unit
+                                        <i class="bi bi-question-circle-fill text-info"
+                                           data-toggle="tooltip" data-placement="top"
+                                           title="Select the appropriate unit required for the product's use.">
+                                        </i>
+                                        <span class="text-danger">*</span>
+                                    </label>
                                     <div class="input-group">
-                                        <select class="form-control" multiple name="product_unit[]" id="product_unit">
-                                            <option value="" selected disabled>Select Unit</option>
-
-                                            @foreach (\Modules\Setting\Entities\Unit::orderBy('name')->get() as $unit)
+                                        {{--<select class="form-control" multiple name="product_unit[]" id="product_unit">
+                                            --}}{{--<option value="" selected disabled>Select Unit</option>--}}{{--
+                                            @foreach (Unit::orderBy('name')->get() as $unit)
                                                 @if (auth()->user()->hasRole('Super Admin'))
-                                                    <option value="{{ $unit->short_name }}">
-                                                        {{ $unit->name . ' | ' . $unit->short_name }}</option>
+                                                    <option
+                                                        value="{{ $unit->short_name }}">
+                                                        {{ $unit->name .' | '. $unit->short_name }}
+                                                    </option>
                                                 @else
                                                     @if ($unit->user_id == auth()->user()->id || $unit->user_id == 1)
-                                                        <option value="{{ $unit->short_name }}">
-                                                            {{ $unit->name . ' | ' . $unit->short_name }}</option>
+                                                        <option
+                                                            value="{{ $unit->short_name }}">
+                                                            {{ $unit->name .' | '. $unit->short_name }}
+                                                        </option>
                                                     @endif
                                                 @endif
                                             @endforeach
-                                        </select>
-                                        <div class="input-group-append d-flex">
-                                            <a href="{{ url('units/create') }}" class="btn btn-outline-primary">
-                                                Add
-                                            </a>
-                                        </div>
+                                        </select>--}}
+
+{{--                                        <select class="form-control" multiple name="product_unit[]" id="product_unit">--}}
+{{--                                            @foreach (Unit::where('user_id', auth()->user()->id)->orWhere('user_id', 1)->orderBy('name')->get() as $unit)--}}
+{{--                                                <option value="{{ $unit->short_name }}">{{ $unit->name .' | '. $unit->short_name }}</option>--}}
+{{--                                            @endforeach--}}
+{{--                                        </select>--}}
+
+
+                                    </div>
+                                    <div class="input-group-append d-flex"
+                                         style="flex-direction: row; justify-content: flex-end;">
+                                        <a href="{{ url('units/create') }}" class="btn btn-outline-primary">Add</a>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label for="product_note">Note</label>
-                                <textarea name="product_note" id="product_note" rows="4 " class="form-control"></textarea>
+                                <textarea name="product_note" id="product_note" rows="4 "
+                                          class="form-control"></textarea>
                             </div>
                         </div>
                     </div>
@@ -143,10 +144,10 @@
                         <div class="card-body">
                             <div class="form-group">
                                 <label for="image">Product Images <i class="bi bi-question-circle-fill text-info"
-                                        data-toggle="tooltip" data-placement="top"
-                                        title="Max Files: 3, Max File Size: 1MB, Image Size: 400x400"></i></label>
+                                                                     data-toggle="tooltip" data-placement="top"
+                                                                     title="Max Files: 3, Max File Size: 1MB, Image Size: 400x400"></i></label>
                                 <div class="dropzone d-flex flex-wrap align-items-center justify-content-center"
-                                    id="document-dropzone">
+                                     id="document-dropzone">
                                     <div class="dz-message" data-dz-message>
                                         <i class="bi bi-cloud-arrow-up"></i>
                                     </div>
@@ -180,11 +181,11 @@
             headers: {
                 "X-CSRF-TOKEN": "{{ csrf_token() }}"
             },
-            success: function(file, response) {
+            success: function (file, response) {
                 $('form').append('<input type="hidden" name="document[]" value="' + response.name + '">');
                 uploadedDocumentMap[file.name] = response.name;
             },
-            removedfile: function(file) {
+            removedfile: function (file) {
                 file.previewElement.remove();
                 var name = '';
                 if (typeof file.file_name !== 'undefined') {
@@ -202,16 +203,16 @@
                 });
                 $('form').find('input[name="document[]"][value="' + name + '"]').remove();
             },
-            init: function() {
+            init: function () {
                 @if (isset($product) && $product->getMedia('images'))
-                    var files = {!! json_encode($product->getMedia('images')) !!};
-                    for (var i in files) {
-                        var file = files[i];
-                        this.options.addedfile.call(this, file);
-                        this.options.thumbnail.call(this, file, file.original_url);
-                        file.previewElement.classList.add('dz-complete');
-                        $('form').append('<input type="hidden" name="document[]" value="' + file.file_name + '">');
-                    }
+                var files = {!! json_encode($product->getMedia('images')) !!};
+                for (var i in files) {
+                    var file = files[i];
+                    this.options.addedfile.call(this, file);
+                    this.options.thumbnail.call(this, file, file.original_url);
+                    file.previewElement.classList.add('dz-complete');
+                    $('form').append('<input type="hidden" name="document[]" value="' + file.file_name + '">');
+                }
                 @endif
             }
         }
@@ -219,7 +220,7 @@
 
     <script src="{{ asset('js/jquery-mask-money.js') }}"></script>
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('#product_cost').maskMoney({
                 prefix: '{{ settings()->currency->symbol }}',
                 thousands: '{{ settings()->currency->thousand_separator }}',
@@ -231,7 +232,7 @@
                 decimal: '{{ settings()->currency->decimal_separator }}',
             });
 
-            $('#product-form').submit(function() {
+            $('#product-form').submit(function () {
                 var product_cost = $('#product_cost').maskMoney('unmasked')[0];
                 var product_price = $('#product_price').maskMoney('unmasked')[0];
                 $('#product_cost').val(product_cost);
