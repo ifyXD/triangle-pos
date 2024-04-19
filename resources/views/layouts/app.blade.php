c17c080fc4ed04afc03ecf99aa88ac0d02305ee0
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2024-04-20 03:34:26 +0800

    ok
    (cherry picked from commit 830639f6032eba735e428fa9891efedcdda73d67)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,25 @@
     <link rel="icon" href="{{ asset('images/favicon.png') }}">
 
     <link rel="stylesheet" href="{{ asset('css/menu-styling.css') }}">
+
     @include('includes.main-css')
+
+{{--    <!-- Font Awesome -->--}}
+{{--    <link--}}
+{{--        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"--}}
+{{--        rel="stylesheet"--}}
+{{--    />--}}
+{{--    <!-- Google Fonts -->--}}
+{{--    <link--}}
+{{--        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"--}}
+{{--        rel="stylesheet"--}}
+{{--    />--}}
+    <!-- MDB -->
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css"
+        rel="stylesheet"
+    />
+
 
 
     <style>
@@ -103,6 +121,12 @@
             }
         });
     </script>
+
+    <!-- MDB -->
+    <script
+        type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.umd.min.js"
+    ></script>
 </body>
 
 </html>
